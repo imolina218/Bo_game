2fdfec64f49e755de7909a43f879b6deced89cf3
imolina218 <31362263+imolina218@users.noreply.github.com>
2020-05-22 23:30:15 -0300

    Add files via upload

---
+++ BO_game/Enemys.py
@@ -0,0 +1,37 @@
+class Enemy:
+    def __init__(self, name, lives, enigmas):
+        self.name = name
+        self.lives = lives
+        self.enigmas = enigmas
+
+    def get_name(self):
+        self.name = name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
